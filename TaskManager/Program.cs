e476a5084a25104af9fe705b60ff0cf4f0c57645
Shadvonic <moxeymarcus28@gmail.com>
2025-02-20 12:01:09 -0500

    Add project files.

---
+++ TaskManager/Program.cs
@@ -0,0 +1,5 @@
+﻿//Task Manager
+//Use a list to store tasks.
+//Implement methods to add, remove, update, and list tasks.
+//Allow users to mark tasks as complete.
+//Use a dictionary to categorize tasks (e.g., Work, Personal, Urgent).
